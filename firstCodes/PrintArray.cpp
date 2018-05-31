3ec7c32f43133a77a222ffdade76028316f1aee1
Yuzhen Ye <yye@yuzhen-yes-mbp.attlocal.net>
2018-05-30 22:40:52 -0400

    switch parameter order

--- firstCodes/PrintArray.cpp
+++ firstCodes/PrintArray.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void PrintArray (int nums[], int length) 
+void PrintArray (int length, int nums[]) 
 {
 	for (int n=0; n<length; n++)
 		cout << nums[n] << ' ';
@@ -13,6 +13,6 @@ int main ()
 {
 	int firstarray[] = {5, 10, 15};
 	int secondarray[] = {2, 4, 6, 8, 10};
-	PrintArray(firstarray,3);
-	PrintArray(secondarray,5);
+	PrintArray(3, firstarray);
+	PrintArray(5, secondarray);
 }
