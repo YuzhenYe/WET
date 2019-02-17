9e8af11a1bd2e1a6d22b8873e57b801dfac28ffe
Yuzhen Ye <yye@yuzhen-yes-mbp.attlocal.net>
2019-02-17 17:14:31 -0500

    add hw for week35

--- basics/pairs.cpp
+++ basics/pairs.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace::std;
-int pairs(int n)
+void enumerate(int n, int *partner, string* names)
 {
-        if(n == 2) return 1;
-        else {
-                return (1 + (n - 2)) * pairs(n - 2);
-        }
+	int i, j;
+	for(i = 0; i < n; i ++) {
+		if(partner[i] == -1) break;
+	}
+	if(i == n) { //all paired
+		for(i = 0; i < n; i ++) {
+			cout<<names[i]<<"-"<<names[partner[i]]<<" ";
+		}
+		cout<<endl;
+		return;
+	}
+	for(j = i + 1; j < n; j ++) {	
+		if(partner[j] != -1) continue;
+		//enumerate each time only forms a pair
+		partner[i] = j;
+		partner[j] = i;
+
+		//and let enumerate() function take care of the rest
+		enumerate(n, partner, names);
+
+		//break pairing between i and j, to let i pair with other unpaired person
+		partner[i] = partner[j] = -1;
+	}
 }
+
 int main()
 {
-	int n;
+	int i, n;
 	cin >> n;
-	cout<<"n " <<n<<" "<<pairs(n)<<endl;
+	string names[100];	
+	for(i = 0; i < n; i ++) cin >> names[i];
+	int partner[100];
+	for(i = 0; i < n; i ++) partner[i] = -1;
+	enumerate(n, partner, names);
 }
